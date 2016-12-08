c86447e7fcaaaf4eb95a7a21ef8c48c308c15b94
John Hallman <jhallman5@gmail.com>
2016-12-07 17:12:01 -0800

    signup.js creation

---
+++ signup.js
@@ -0,0 +1,14 @@
+var User = require('my-superior-spider-test/models/User');
+
+
+submit.onclick = function() {
+    User.add({name.type = document.getElementByID("username"),
+              email.type = document.getElementByID("email"),
+              password.type = document.getElementByID("password")
+            });
+}
+
+
+
+
+  module.exports ={submit.onclick};
