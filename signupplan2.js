6d19da26aca69080c5e01eac6967bfaf198b7fae
John Hallman <jhallman5@gmail.com>
2016-12-08 11:00:24 -0800

    singupplan2 not returning undefined for the first time

--- signupplan2.js
+++ signupplan2.js
@@ -4,37 +4,13 @@ function user(username, email, password) {
   this.password = password;
 }
 var users =[];
-function users(username, email, password) {
-  for user in users {
-    if user.username == true;
-     && user.email == true;
-     && user.password == true;
-     
-  }
+
+function createUser() {
+  var userInBlock = document.getElementById("username");
+  userInblock = new user(document.getElementByID(username),
+                                document.getElementByID(email),
+                                document.getElementByID(password)
+                              );
+  return users.push(userInBlock);
 }
-
-
-document.getElementByID(.submit).onclick = users.push(new user(document.getElementByID(username),
-                                                  document.getElementByID(email),
-                                                  document.getElementByID(password)
-                                                ));
-
-
-
-onclick event -> user (var this.username)
-
-var this.username = new user(.username, .email, .password);
-
-
-
-
-
-var john = new user (jh, jh@gmail, 1234);
-john.username = jh
-john.email = jh@gmail
-john.password = 1234;
-
-var john.pr.53.username = jh
-john.prototype.email = jh@gmail.com
-
-console.log(john,)
+console.log(users);
