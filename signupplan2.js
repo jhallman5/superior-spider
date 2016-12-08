63c1c0848037e2c30ace3c27dfb5865664a3e86c
John Hallman <jhallman5@gmail.com>
2016-12-08 10:14:44 -0800

    making the constructor function

---
+++ signupplan2.js
@@ -0,0 +1,40 @@
+function user(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+var users =[];
+function users(username, email, password) {
+  for user in users {
+    if user.username == true;
+     && user.email == true;
+     && user.password == true;
+     
+  }
+}
+
+
+document.getElementByID(.submit).onclick = users.push(new user(document.getElementByID(username),
+                                                  document.getElementByID(email),
+                                                  document.getElementByID(password)
+                                                ));
+
+
+
+onclick event -> user (var this.username)
+
+var this.username = new user(.username, .email, .password);
+
+
+
+
+
+var john = new user (jh, jh@gmail, 1234);
+john.username = jh
+john.email = jh@gmail
+john.password = 1234;
+
+var john.pr.53.username = jh
+john.prototype.email = jh@gmail.com
+
+console.log(john,)
