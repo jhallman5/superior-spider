0dcef46fbb64f63fed68530df15a8f362f735e17
John Hallman <jhallman5@gmail.com>
2016-12-08 17:55:53 -0800

    end of the day Thursday

--- signupplan2.js
+++ signupplan2.js
@@ -1,16 +1,23 @@
-function user(username, email, password) {
+var {jQuery} = require('jquery-3.1.1.min.js');
+
+function User(username, email, password) {
   this.username = username;
   this.email = email;
   this.password = password;
 }
 var users =[];
+var loggedInUser;
 
-$(document).on("click", "#submit",function createUser() {
+$('#submit').on('click',function createUser() {
   var newUser;
-  newUser = new user($("username").val(),
-                         $("email").val(),
-                         $("password").val()
-                      );
+  var username = $("#username").val();
+  var email = $("#email").val();
+  var password = $("#username").val();
+  newUser = new User(username, email, password);
   users.push(newUser);
+  loggedInUser = newUser;
   return users;
 })
+
+module.exports ={users,
+                loggedInUser};
